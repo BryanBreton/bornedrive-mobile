470d225aa5ab46b7006e446b79db8fb91142bcb6
Bryan Breton <bryan.breton@systeme-u.fr>
2020-11-24 12:07:22 +0100

    taille col

--- src/views/Commandes.vue
+++ src/views/Commandes.vue
@@ -1,71 +1,76 @@
 <template>
   <div class="all">
     <v-app-bar class="bar">
-      <v-col col="1"><img src="../assets/logo.png" alt="logo"></v-col>
+      <v-col col="1"><img src="../assets/logo.png" alt="logo" /></v-col>
       <v-col col="5"> U - Livreur</v-col>
       <v-col col="6">{{ prenom }}</v-col>
     </v-app-bar>
     <v-container>
       <v-row v-for="commande in commandes" :key="commande.id">
-          <v-container>
-              <v-card :class="[commande.statut === 3 ? 'gray' : commande.borne.color, 'testage']" @click="distribuer(commande.id)" elevation="1" outlined shaped style="width: 100% !important">
-                <v-row>
-                  <v-col cols="2" class="numBorne">{{ commande.borne.id }}</v-col>
-                <v-col cols="6" class="d-flex align-end mb-6">{{ commande.client.civilite }} {{ commande.client.nom }} {{ commande.client.prenom }}</v-col>
-                <v-col cols="4" class="numCommande">{{ commande.codeCourt }}</v-col>
-                </v-row>
-                
-              </v-card>
-          </v-container>
-          
+        <v-container>
+          <v-card
+            :class="[commande.statut === 3 ? 'gray' : commande.borne.color, 'testage']"
+            @click="distribuer(commande.id)"
+            elevation="1"
+            outlined
+            shaped
+            style="width: 100% !important"
+          >
+            <v-row>
+              <v-col cols="2" class="numBorne">{{ commande.borne.id }}</v-col>
+              <v-col cols="5" class="d-flex align-end mb-6">{{ commande.client.civilite }} {{ commande.client.nom }} {{ commande.client.prenom }}</v-col>
+              <v-col cols="5" class="numCommande">{{ commande.codeCourt }}</v-col>
+            </v-row>
+          </v-card>
+        </v-container>
       </v-row>
     </v-container>
   </div>
 </template>
 <script>
-import axios from 'axios'
+import axios from "axios"
 
 export default {
-  data(){
-    return{
+  data() {
+    return {
       commandes: [],
-      prenom : ""
+      prenom: "",
     }
   },
-  methods:{
-    distribuer(idCommande){
-        axios.get('http://' + window.location.hostname + ':3001/commandes/'+idCommande+'/preparateur/'+localStorage.id)
+  methods: {
+    distribuer(idCommande) {
+      axios.get("http://" + window.location.hostname + ":3001/commandes/" + idCommande + "/preparateur/" + localStorage.id)
     },
-    getDatas(){
-        axios.get('http://' + window.location.hostname + ':3001/commandes/toPick').then(r => {
-          this.commandes = r.data
-        })
-        setTimeout(() => {
-            this.getDatas()
-        }, 3000);
-    }
+    getDatas() {
+      axios.get("http://" + window.location.hostname + ":3001/commandes/toPick").then((r) => {
+        this.commandes = r.data
+      })
+      setTimeout(() => {
+        this.getDatas()
+      }, 3000)
+    },
   },
-  mounted(){
-      this.prenom = localStorage.prenom
-      this.getDatas()
-  }
+  mounted() {
+    this.prenom = localStorage.prenom
+    this.getDatas()
+  },
 }
 </script>
 
 <style>
-.bar{
-    background-color: #0077b2 !important;
+.bar {
+  background-color: #0077b2 !important;
 }
-.all{
-    height: 100% !important;
+.all {
+  height: 100% !important;
 }
-.numBorne{
+.numBorne {
   font-size: 300% !important;
 }
-.numCommande{
+.numCommande {
   font-size: 300% !important;
 }
-.statut-3{
+.statut-3 {
   background-color: gray !important;
 }
 </style>
